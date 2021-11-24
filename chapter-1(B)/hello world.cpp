e06ca28754c638994d91a0b59e0afc660f092c42
vipin kumar <kumarvipin24031999@gmail.com>
2021-11-24 17:34:31 +0530

    escape sequence

---
+++ chapter-1(B)/hello world.cpp
@@ -0,0 +1,19 @@
+/* writing program  for hello world   */
+#include<stdio.h>
+int main()
+{
+	printf("hello world");
+}
+/*preprocessor directives(#)
+global varibles
+int main()
+{
+	local varibles
+	statements
+	.
+	.
+	.
+	.
+	return 0;
+}*/
+
---
+++ chapter-2(Escape Sequence)/hello world.cpp
@@ -0,0 +1,21 @@
+/* writing program  for hello world   */
+#include<stdio.h> 
+#include<conio.h>
+int main()
+{
+	printf("hello world \t ,jbefkewbf kwjfhkewjf kwjfhejf ");   // 	\character 
+ 	getch();
+}
+/*preprocessor directives(#)
+global varibles
+int main()
+{
+	local varibles
+	statements
+	.
+	.
+	.
+	.
+	return 0;
+}*/
+
